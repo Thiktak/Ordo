110e165aac567b7d967e97e043998b8304e0a97d
Georges Olivarès <olivares.georges@gmail.com>
2011-11-28 23:37:13 +0100

    Modification générale de la présentation du membre (/show)  - Version non définitive (CSS inter View)  + gestiondes données condidentielles pour les Admins  @todo infos visibles pour certains groupes/responsables

--- apps/frontend/modules/annuaire/templates/showSuccess.php
+++ apps/frontend/modules/annuaire/templates/showSuccess.php
@@ -16,7 +16,7 @@ function getClass($filiere)
   }
 }
 ?>
-<article>
+<article class="page-annuaire page-annuaire-index">
   <header>
     <h1>
       <?php echo $membre; ?>
@@ -50,101 +50,128 @@ function getClass($filiere)
     </aside>
   </header>
 
+  <style>
+    .page-annuaire-index figure { /*position: absolute; top: 50px; left: 10px;*/ float: left; width: 200px; text-align: center; }
+    .page-annuaire-index figure img { max-width: 100px; max-height: 150px; margin: 10px; }
+    .page-annuaire-index figure figcaption > * { background-color: #eaeaea; display: block; padding: 5px; margin: 10px; }
+    
+    .page-annuaire-index .vcard { /*position: absolute; top: 50px; left: 220px; right: 250px;*/ margin: 0 250px; margin-left: 220px; background-color: #f5f5f5; box-shadow: 0 0 5px #eaeaea; padding: 2em; }
+    .page-annuaire-index .vcard h2 { font-size: 1.5em; margin-bottom: 1em; }
+    
+    .page-annuaire-index .other { /*position: absolute; top: 50px;*/ float: right; width: 220px; right: 10px; }
+    .page-annuaire-index .other dt { margin-top: 10px; color: Gray; }
+    .page-annuaire-index .other dd { text-align: right; }
+    
+    
+    
+    .page-annuaire-index .vcard dt { display: inline-block; margin: .5%; padding: .5%; width: 20%; vertical-align: top; text-align: right; color: Gray; }
+    .page-annuaire-index .vcard dd { display: inline-block; margin: .5%; padding: .5%; padding-left: 1%; width: 70%; min-height: 1em; border-left: 4px solid White; }
+    .page-annuaire-index .vcard dd:hover { background-color: White; border-color: #eaeaea; }
+    .page-annuaire-index .list { display: block; }
+    
+    span.datas:empty { background-color: rgba(150, 150, 150, .1); display: inline-block; margin: 1px 2px; width: 100px; height: 1em; padding: 0 2px; }
+    .isAdmin, .isAdmin * { color: Orange; font-weight: bold; }
+    .clear { clear: both; height: 0; }
+  </style>
 
   <div id='annuaire.show.tableauID'>
     
-    <?php echo image_tag('/uploads/annuaire/' . $membre->getPhoto(), array('style' => 'float: left; margin: 10px;')); ?>
+    <figure>
+      <span class="datas"><?php echo image_tag('/uploads/annuaire/' . $membre->getPhoto()); ?>
+      <figcaption>
+        <span class="status"><span class="datas"><?php echo $membre->getStatus(); ?></span></span>
+        <span class="filiere <?php echo getClass($membre->getFiliere()) ?>"><span class="datas"><?php echo $membre->getFiliere(); ?></span></span>
+        <span class="poste"><span class="datas"><?php echo $membre->getPoste(); ?></span></span>
+      </figcaption>
+    </figure>
     
-    <table>
-    <tbody>
-      <tr>
-      <th>Login</th>
-      <td><?php echo $membre->getUsername() ?></td>
-      <th>Numéro étudiant</th>
-      <td><?php echo $membre->getNumeroEtudiant() ?></td>
-      </tr>
-      <tr>
-      <th>Nom</th>
-      <td><?php echo $membre->getNom() ?></td>
-      <th>Poste</th>
-      <td><?php echo $membre->getPoste() ?></td>
-      </tr>
-      <tr>
-      <th>Prenom</th>
-      <td><?php echo $membre->getPrenom() ?></td>
-      <th>Filière</th>
-      <td class='<?php echo getClass($membre->getFiliere()) ?>'><?php echo $membre->getFiliere() ?></td>
-      </tr>
-      <tr>
-      <th>Adresse locale</th>
-      <td><?php echo nl2br($membre->getAdresseMulhouse()) ?><br/>
-        <?php echo $membre->getCpMulhouse() ?><br/>
-        <?php echo $membre->getVilleMulhouse() ?>
-      </td>
-      <th>Adresse des parents</th>
-      <td><?php echo nl2br($membre->getAdresseParents()) ?><br/>
-        <?php echo $membre->getCpParents() ?><br/>
-        <?php echo $membre->getVilleParents() ?>
-      </td>
-      </tr>
-      <tr>
-      <th>Sexe</th>
-      <td><?php echo $membre->getSexe() ?></td>
-      <th>Téléphone portable</th>
-      <td><?php echo $membre->getTelMobile() ?></td>
-      </tr>
-      <tr>
-      <th>Date de naissance</th>
-      <td><?php echo format_date($membre->getDateNaissance()) ?></td>
-      <th>Téléphone fixe</th>
-      <td><?php echo $membre->getTelFixe() ?></td>
-      </tr>
-      <tr>
-      <th>Ville de naissance</th>
-      <td><?php echo $membre->getVilleNaissance() ?></td>
-      <th>Email IARISS</th>
-      <td><?php echo $membre->getEmailInterne() ?></td>
-      </tr>
-      <tr>
-      <th>Numero de sécu</th>
-      <td><?php echo $membre->getNumeroSecu() ?></td>
-      <th>Email externe</th>
-      <td><?php echo $membre->getEmailExterne() ?></td>
-      </tr>
-      <tr>
-      <th>Status pour l'annuaire</th>
-      <td><?php echo $membre->getStatus() ?></td>
-      <th>Promotion</th>
-      <td><?php echo $membre->getPromo() ?></td>
-      </tr>
-      <tr>
-      <th>Fiche crée le</th>
-      <td><?php echo format_date($membre->getCreatedAt()) ?></td>
-      <th>Dernière mise à jour le</th>
-      <td><?php echo format_date($membre->getUpdatedAt()) ?></td>
-      </tr>
-      <tr>
-      <th>Cotisations</th>
-      <td>
-        <?php 
+  
+    <div class="other">
+      <dl>
+        <?php if( $user->isAdmin() ): ?>
+        <dt class="isAdmin">Numéro étudiant</dt>
+        <dd class="isAdmin"><span class="datas"><?php echo $membre->getNumeroEtudiant(); ?></span></dd>
+        
+        <dt class="isAdmin">Numéro de sécu</dt>
+        <dd class="isAdmin"><span class="datas"><?php echo $membre->getNumeroSecu(); ?></span></dd>
+        <?php endif; ?>
+        
+        <dt>Fiche crée le</dt>
+        <dd><span class="datas"><?php echo format_date($membre->getCreatedAt()) ?></span></dd>
+        
+        <dt>Dernière mise à jour</dt>
+        <dd><span class="datas"><?php echo format_date($membre->getUpdatedAt()) ?></span></dd>
+        
+        <dt>Cotisations</dt>
+        <dd>
+        <span class="datas"><?php
         $cotisation_annee = array();
         foreach($membre->getCotisations() as $cotisation)
         $cotisation_annee[] = $cotisation->__toString();
-        echo implode(',', $cotisation_annee) ?>
-      </td>
-      <th>Quittances</th>
-      <td>
-        <?php 
+        echo implode('</span>, <span class="datas">', $cotisation_annee) ?></span>
+        </dd>
+        
+        <dt>Quittances</dt>
+        <dd>
+        <span class="datas"><?php
         $quittance_annee = array();
         foreach($membre->getQuittances() as $quittance)
         $quittance_annee[] = $quittance->__toString();
-        echo implode(',', $quittance_annee) ?>
-      </td>
-      </tr>
-    </tbody>
-    </table>
-  </div>
-
+        echo implode(',', $quittance_annee) ?></span>
+        </dd>
+      </dl>
+    </div>
+    
+    <div class="vcard">
+      <h2><span class="datas"><?php echo $membre->getNom(); ?></span> <span class="datas"><?php echo $membre->getPrenom(); ?></span> (<span class="datas"><?php echo $membre->getUsername(); ?></span>)</h2>
+      <dl>
+        <dt></dt>
+        <dd>
+          <span class="datas"><?php echo $membre->getSexe(); ?></span>
+          - né le <span class="datas"><?php echo $membre->getDateNaissance(); ?></span>
+          - à <span class="datas"><?php echo $membre->getVilleNaissance(); ?></span>
+        </dd>
+        
+        <dt>Adresse :</dt>
+        <dd>
+          <span class="datas"><?php if( $membre->getAdresseMulhouse() ): ?>
+            <span class="datas"><?php echo nl2br($membre->getAdresseMulhouse()) ?></span><br/>
+            <span class="datas"><?php echo $membre->getCpMulhouse(); ?></span><br/>
+            <span class="datas"><?php echo $membre->getVilleMulhouse(); ?></span>
+            <?php endif; ?></span>
+        </dd>
+        
+        <dt>Adresse (parents) :</dt>
+        <dd>
+          <span class="datas"><?php if( $membre->getAdresseParents() . $membre->getVilleParents() ): ?>
+            <span class="datas"><?php echo nl2br($membre->getAdresseParents()) ?></span><br/>
+            <span class="datas"><?php echo $membre->getCpParents(); ?></span><br/>
+            <span class="datas"><?php echo $membre->getVilleParents(); ?></span>
+            <?php endif; ?></span>
+        </dd>
+        
+        <dt>Téléphone :</dt>
+        <dd>
+          <span class="list"><span class="datas"><?php echo $membre->getTelMobile(); ?></span>
+          <span class="list"><span class="datas"><?php echo $membre->getTelFixe(); ?></span>
+        </dd>
+        </dd>
+        
+        <dt>Email :</dt>
+        <dd>
+          <span class="list"><span class="datas"><?php echo $membre->getEmailExterne(); ?></span>
+          <span class="list"><span class="datas"><?php echo $membre->getEmailInterne(); ?></span>@iariss.fr
+        </dd>
+        
+        <dt>Promotion :</dt>
+        <dd><span class="datas"><?php echo $membre->getPromo(); ?></span></dd>
+      </dl>
+      <br class="clear" />
+    </div>
+    
+    <br class="clear" />
+    <hr />
+    
   <?php
   $carteID = $membre->getCarteID();
   $justif= $membre->getJustDomicile();
